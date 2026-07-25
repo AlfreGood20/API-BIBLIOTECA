e7b3343fa8e4d0d83ef18e7408792be5c26b69a4
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-07-25 16:09:07 -0600

    BUSCAR RESERVAS DE UN USUARIO POR ORDEN DESENDIENTE POR FECHARESERVA

--- src/main/java/com/api/biblioteca/repositorys/ReservaRepository.java
+++ src/main/java/com/api/biblioteca/repositorys/ReservaRepository.java
@@ -3,6 +3,8 @@ package com.api.biblioteca.repositorys;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.api.biblioteca.models.Reserva;
 import com.api.biblioteca.models.Usuario;
@@ -10,5 +12,11 @@ import com.api.biblioteca.models.Usuario;
 @Repository
 public interface ReservaRepository extends JpaRepository<Reserva, Long> {
 
-    List<Reserva> findByUsuario(Usuario usuario);
+    List<Reserva> findByUsuarioOrderByFechaReservaDesc(Usuario usuario);
+
+    @Query("""
+        SELECT r FROM Reserva r
+        WHERE (:estadoId IS NULL OR r.estado.id = :estadoId)
+        """)
+    List<Reserva> buscarPorParametros(@Param("estadoId") Long estadoId);
 }
