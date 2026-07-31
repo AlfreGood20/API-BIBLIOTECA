8f3e4b9bfaa203c7520d24d024e80a6342273a38
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-07-30 18:09:07 -0600

    DOCUMENTACION PARA RESERVAS DE LIBROS

--- src/main/java/com/api/biblioteca/controllers/ReservaController.java
+++ src/main/java/com/api/biblioteca/controllers/ReservaController.java
@@ -8,6 +8,10 @@ import com.api.biblioteca.dtos.request.ReservaRequest;
 import com.api.biblioteca.dtos.response.ReservaResponse;
 import com.api.biblioteca.dtos.updates.EstadoRequest;
 import com.api.biblioteca.services.ReservaService;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import java.util.List;
@@ -22,14 +26,17 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
+@Hidden
 @RestController
 @RequestMapping("api/reservas")
 @RequiredArgsConstructor
+@Tag(name = "Reservas", description = "Operaciones Reserva de libros.")
 public class ReservaController {
 
     private final ReservaService reservaService;
 
+    /* PARA USUARIOS */
+    @Operation(summary = "Crea nueva reserva", description = "Crea nueva reserva para usuarios autenticados. Pidiendo asi como unico valor el ID del libro.")
     @PostMapping("/usuario")
     public ResponseEntity<ReservaResponse> crearNuevo(
         @Valid @RequestBody ReservaRequest request, @AuthenticationPrincipal CustomUserDetails usuario) {
@@ -37,16 +44,27 @@ public class ReservaController {
         return new ResponseEntity<ReservaResponse>(reservaService.crearNuevo(request, usuario), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Obtener mis reserva", description = "Devuelve el historial de reserva para usuarios autenticados. Tanto pendiente, cancelados, expirado o disponibles.")
     @GetMapping("/usuario")
     public ResponseEntity<List<ReservaResponse>> misReservas(@AuthenticationPrincipal CustomUserDetails usuario) {
         return ResponseEntity.ok(reservaService.misReservas(usuario));
     }
 
+    @Operation(summary = "Cambiar estado cancelar reserva", description = """
+                Cambias el estado de una reserva para usuario autenticado. 
+                Deberas de indicar el ID de reserva. 
+                No podras cambiar el estado de una reserva que no le pertenezca.
+            """)
     @PatchMapping("/usuario/{id}")
     public ResponseEntity<ReservaResponse> cambiarEstadoCancelar(@AuthenticationPrincipal CustomUserDetails usuario, @PathVariable Long id){
         return ResponseEntity.ok(reservaService.cancelarReserva(usuario, id));
     }
 
+
+
+
+    /* BIBLIOTECARIOS */
+    @Operation(summary = "Obtener reservas", description = "Obtendras un listado de la reservas, solo para bibliotecarios y administradores.")
     @GetMapping("/bibliotecario")
     public ResponseEntity<List<ReservaResponse>> obtenerReservas(
         @RequestParam(required = false) Long estadoId
@@ -54,20 +72,27 @@ public class ReservaController {
         return ResponseEntity.ok(reservaService.obtenerReservas(estadoId));
     }
 
+    @Operation(summary = "Obtener reserva por id", description = "Obtendras una reserva especifica por ID, solo para bibliotecarios y administradores.")
     @GetMapping("/bibliotecario/{id}")
     public ResponseEntity<ReservaResponse> obtenerPorId(@PathVariable Long id) {
         return ResponseEntity.ok(reservaService.obtenerReservaPorId(id));
     }
 
+    @Operation(summary = "Cambiar estado de una reserva por id", description = "Cambiaras el estado de una reserva especificando el ID de la reserva, solo para bibliotecarios y administradores.")
+    @PatchExchange("/bibliotecario/{id}")
+    public ResponseEntity<ReservaResponse> cambiarEstado(@PathVariable Long id, @RequestBody EstadoRequest request, @AuthenticationPrincipal CustomUserDetails usuarioAdmin){
+        return ResponseEntity.ok(reservaService.cambiarEstadoReserva(id, request, usuarioAdmin));
+    }
+
+
+    
+
+    /* ADMINISTRADORES */
+    @Operation(summary = "Eliminar reserva por id", description = "Eliminaras reserva de la base de datos por ID, solo para administradores.")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> eliminarPorId(@PathVariable Long id){
         reservaService.eliminarReservaPorId(id);
         return ResponseEntity.noContent().build();
-    }
-
-    @PatchExchange("/bibliotecario/{id}")
-    public ResponseEntity<ReservaResponse> cambiarEstado(@PathVariable Long id, @RequestBody EstadoRequest request, @AuthenticationPrincipal CustomUserDetails usuarioAdmin){
-        return ResponseEntity.ok(reservaService.cambiarEstadoReserva(id, request, usuarioAdmin));
     }
 
 }
