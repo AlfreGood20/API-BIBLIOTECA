7a92af2cf2e0202c4c43269793feeab7db67f6c8
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-07-30 18:03:46 -0600

    DOCUMENTACION PARA CONTROLADOR MUNICIPIOS PARA USUARIOS

--- src/main/java/com/api/biblioteca/controllers/MunicipioController.java
+++ src/main/java/com/api/biblioteca/controllers/MunicipioController.java
@@ -4,6 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.api.biblioteca.models.Municipio;
 import com.api.biblioteca.services.MunicipioService;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import java.util.List;
@@ -15,29 +19,37 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
+@Hidden
 @RestController
 @RequestMapping("api/municipios")
 @RequiredArgsConstructor
+@Tag(name = "Municipios", description = "Operaciones para municipios usuarios.")
 public class MunicipioController {
 
     private final MunicipioService municipioService;
 
-    @PostMapping("/")
-    public ResponseEntity<Municipio> crearNuevo(@Valid @RequestBody Municipio request) {
-        return new ResponseEntity<Municipio>(municipioService.crearNuevo(request), HttpStatus.CREATED);
-    }
-
-    @GetMapping("/")
+    /* PUBLICOS */
+    @Operation(summary = "Obtener municipios", description = "Obtendras todos los municipios. Para publicos.")
+    @GetMapping("/public")
     public ResponseEntity<List<Municipio>> obtenerMunicipios() {
         return ResponseEntity.ok().body(municipioService.obtenerMunicipios());
     }
+
     
+    /* ADMINISTRADORES */
+    @Operation(summary = "Crear nuevo municipio", description = "Crear nuevo municipio, solo para administradores.")
+    @PostMapping
+    public ResponseEntity<Municipio> crearNuevo(@Valid @RequestBody Municipio request) {
+        return new ResponseEntity<Municipio>(municipioService.crearNuevo(request), HttpStatus.CREATED);
+    }
+    
+    @Operation(summary = "Obtener municipio por id", description = "Obtendras un municipio por id, solo para administradores")
     @GetMapping("/{id}")
     public ResponseEntity<Municipio> obtenerPorId(@PathVariable Long id) {
         return ResponseEntity.ok().body(municipioService.obtenerMunicipioPorId(id));
     }
 
+    @Operation(summary = "Eliminar municipio por id", description = "Eliminaras un municpio por id, solo para administradores")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> eliminarPorId(@PathVariable Long id){
         municipioService.eliminarMunicipioPorId(id);
