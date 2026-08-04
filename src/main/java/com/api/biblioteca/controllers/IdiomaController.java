d9e9c6b5d83be477ca087e03036a955e803f63e0
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-08-04 17:02:27 -0600

    DOCUMENTACION PARA IDIOMAS DE LIBROS

--- src/main/java/com/api/biblioteca/controllers/IdiomaController.java
+++ src/main/java/com/api/biblioteca/controllers/IdiomaController.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.api.biblioteca.dtos.request.IdiomaRequest;
 import com.api.biblioteca.models.Idioma;
 import com.api.biblioteca.services.IdiomaService;
-
-import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -20,7 +18,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@Hidden
 @RestController
 @RequestMapping("/api/idiomas")
 @RequiredArgsConstructor
