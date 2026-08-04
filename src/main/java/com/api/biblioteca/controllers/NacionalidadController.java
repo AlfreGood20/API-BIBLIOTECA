889bd3435314bd7dc40ccd0653590dcabaf3bee7
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-08-04 17:11:18 -0600

    DOCUMENTACION PARA NACIONALIDAD DE AUTORES

--- src/main/java/com/api/biblioteca/controllers/NacionalidadController.java
+++ src/main/java/com/api/biblioteca/controllers/NacionalidadController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.api.biblioteca.dtos.request.NacionalidadRequest;
 import com.api.biblioteca.models.Nacionalidad;
 import com.api.biblioteca.services.NacionalidadService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import java.util.List;
@@ -18,32 +21,36 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
-@RequestMapping("api/nacionalidades")
+@RequestMapping("/api/nacionalidades")
 @RequiredArgsConstructor
+@Tag(name = "Nacionalidades", description = "Operaciones para nacionalidad de autores.")
 public class NacionalidadController {
 
     private final NacionalidadService nacionalidadService;
 
-    @PostMapping("/")
+    @Operation(summary = "Crear nuevo nacionalidad", description = "Creas una nueva nacionalidad, solo para bibliotecarios y administradores.")
+    @PostMapping
     public ResponseEntity<Nacionalidad> crearNuevo(@Valid @RequestBody NacionalidadRequest request) {
         return new ResponseEntity<Nacionalidad>(nacionalidadService.crearNueva(request), HttpStatus.CREATED);
     }
 
-    @GetMapping("/")
+    @Operation(summary = "Obtener nacionalidades", description = "Obtienes todos las nacionalidades registradas. Solo para biblioticarios y administradores.")
+    @GetMapping
     public ResponseEntity<List<Nacionalidad>> obtenerNacionalidades() {
         return ResponseEntity.ok().body(nacionalidadService.obtenerNacionalidades());
     }
     
+    @Operation(summary = "Obtener nacionalidad por id", description = "Obtienes una nacionalidad por id. Solo para biblioticarios y administradores.")
     @GetMapping("/{id}")
     public ResponseEntity<Nacionalidad> obtenerPorId(@PathVariable Long id) {
         return ResponseEntity.ok().body(nacionalidadService.obtenerNacionalidadPorId(id));
     }
 
+    @Operation(summary = "Eliminar nacionalidad por id", description = "Eliminas una nacionalidas por id. Solo para administradores.")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> eliminarPorId(@PathVariable Long id){
         nacionalidadService.eliminarNacionalidadPorId(id);
         return ResponseEntity.noContent().build();
     }
     
-
 }
