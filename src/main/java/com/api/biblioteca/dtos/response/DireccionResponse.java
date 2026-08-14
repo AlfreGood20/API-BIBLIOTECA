25fa7a6ef91f8de3af45b128b261d8eea84df325
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-08-13 20:25:52 -0600

    ARREGLO DE ATRIBUTOS EN JSON

--- src/main/java/com/api/biblioteca/dtos/response/DireccionResponse.java
+++ src/main/java/com/api/biblioteca/dtos/response/DireccionResponse.java
@@ -1,13 +1,18 @@
 package com.api.biblioteca.dtos.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record DireccionResponse
 (
     Long id,
     String calle,
     String colonia,
+    @JsonProperty("codigo_postal")
     String codigoPostal,
     String municipio,
+    @JsonProperty("numero_exterior")
     String numeroExterior,
+    @JsonProperty("numero_interior")
     String numeroInterior
 ) {
 }
