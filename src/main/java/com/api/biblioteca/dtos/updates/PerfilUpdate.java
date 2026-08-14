ca1b04b259443f1a4bdb75759abec14150d0af69
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-08-13 22:04:02 -0600

    ARREGLADO LOS ATRIBUTOS EN JSON

--- src/main/java/com/api/biblioteca/dtos/updates/PerfilUpdate.java
+++ src/main/java/com/api/biblioteca/dtos/updates/PerfilUpdate.java
@@ -1,6 +1,7 @@
 package com.api.biblioteca.dtos.updates;
 
 import java.util.List;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
@@ -10,8 +11,10 @@ public record PerfilUpdate
     @NotBlank(message = "Es obligatorio")
     String nombre,
     @NotBlank(message = "Es obligatorio")
+    @JsonProperty("apellido_paterno")
     String apellidoPaterno,
     @NotBlank(message = "Es obligatorio")
+    @JsonProperty("apellido_materno")
     String apellidoMaterno,
     @Valid
     @NotEmpty(message = "Es obligatorio")
