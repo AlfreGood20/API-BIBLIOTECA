fb847666c36704dcd93f2422ea6aee128fbb9399
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-08-15 16:35:12 -0600

    ARREGLO DE SERVICIO PARA RESERVA PODER DAR LA FECHA DE LIMITE DE ENTRGA PARA ESTADOS DISPONIBLES.

--- src/main/java/com/api/biblioteca/services/impl/ReservaServiceImpl.java
+++ src/main/java/com/api/biblioteca/services/impl/ReservaServiceImpl.java
@@ -3,7 +3,6 @@ package com.api.biblioteca.services.impl;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Set;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -52,7 +51,45 @@ public class ReservaServiceImpl implements ReservaService{
     private final EstadoEjemplarRepository estadoEjemplarRepository;
 
 
+    /* =============================== SERVICIOS PARA APP WEB RESERVAS ====================================*/
+   @Override
+    public PaginaResponse<ReservaResponse> misReservas(CustomUserDetails usuario, EstadoReservaNombre estado, Pageable pageable) {
+        Page<Reserva> reservas = reservaRepository.buscarMisReservas(usuario.getUsuario(), estado, pageable);
+        Page<ReservaResponse> paginaResponse = reservas.map(reservaMapper::entityToDto);
+        
+        return new PaginaResponse<>(
+            paginaResponse.getContent(), 
+            paginaResponse.getNumber(), 
+            paginaResponse.getTotalPages(), 
+            paginaResponse.getTotalElements(), 
+            paginaResponse.isFirst(), 
+            paginaResponse.isLast());
+    }
+
+    
     @Override
+    @Transactional
+    public ReservaResponse cancelarReserva(CustomUserDetails usuario, Long id) {
+
+        Reserva reserva = buscarReservaPorId(id);
+
+        if(!reserva.getUsuario().getId().equals(usuario.id())){
+            throw new AccessDeniedException("No tienes permiso para esta accion.");
+        }
+
+        if(reserva.getEstado().getNombre() != EstadoReservaNombre.DISPONIBLE && reserva.getEstado().getNombre() != EstadoReservaNombre.PENDIENTE){
+            throw new ConflictExeption("Esta reserva ya se encuentra "+reserva.getEstado().getNombre().name());
+        }
+
+        reserva.setEstado(buscarEstadoReservaPorNombre(EstadoReservaNombre.CANCELADA));
+        return reservaMapper.entityToDto(reservaRepository.save(reserva));
+    }
+
+
+    
+    /* ======================== SERVICIOS PARA ADMIN USUARIO ================================ */
+    @Override
+    @Transactional
     public ReservaResponse crearNuevo(ReservaRequest request, CustomUserDetails usuario) {
 
         EstadoReserva estadoReserva = buscarEstadoReservaPorNombre(EstadoReservaNombre.PENDIENTE);
@@ -73,60 +110,6 @@ public class ReservaServiceImpl implements ReservaService{
         return reservaMapper.entityToDto(reservaRepository.save(reserva));
     }
 
-    /* BUSCAR LAS RESERVAS POR FILTROS */
-   @Override
-    public PaginaResponse<ReservaResponse> misReservas(CustomUserDetails usuario, EstadoReservaNombre estado, Pageable pageable) {
-        Page<Reserva> reservas = reservaRepository.buscarMisReservas(usuario.getUsuario(), estado, pageable);
-        Page<ReservaResponse> paginaResponse = reservas.map(reservaMapper::entityToDto);
-        
-        return new PaginaResponse<>(
-            paginaResponse.getContent(), 
-            paginaResponse.getNumber(), 
-            paginaResponse.getTotalPages(), 
-            paginaResponse.getTotalElements(), 
-            paginaResponse.isFirst(), 
-            paginaResponse.isLast());
-    }
-
-    
-    @Override
-    public ReservaResponse cancelarReserva(CustomUserDetails usuario, Long id) {
-
-        Reserva reserva = buscarReservaPorId(id);
-
-        if(!reserva.getUsuario().getId().equals(usuario.id())){
-            throw new AccessDeniedException("No tienes permiso para esta accion.");
-        }
-
-        if(reserva.getEstado().getNombre() != EstadoReservaNombre.DISPONIBLE && reserva.getEstado().getNombre() != EstadoReservaNombre.PENDIENTE){
-            throw new ConflictExeption("Esta reserva ya se encuentra "+reserva.getEstado().getNombre().name());
-        }
-
-        reserva.setEstado(buscarEstadoReservaPorNombre(EstadoReservaNombre.CANCELADA));
-
-        return reservaMapper.entityToDto(reservaRepository.save(reserva));
-    }
-
-    @Override
-    public List<ReservaResponse> obtenerReservas(Long estadoId) {
-        return reservaMapper.listEntityToListDto(reservaRepository.buscarPorParametros(estadoId));
-    }
-    
-    @Override
-    public ReservaResponse obtenerReservaPorId(Long id) {
-        return reservaMapper.entityToDto(reservaRepository.findById(id)
-            .orElseThrow(() -> new ResourceNotFoundException("Reserva no encontrada"))
-        );
-    }
-
-    @Override
-    public void eliminarReservaPorId(Long id) {
-
-        Reserva reserva = reservaRepository.findById(id)
-            .orElseThrow(() -> new ResourceNotFoundException("Reserva no encontrada."));
-
-        reservaRepository.delete(reserva);
-    }
 
     @Override
     @Transactional
@@ -143,11 +126,12 @@ public class ReservaServiceImpl implements ReservaService{
         }
 
         if(estadoReserva.getNombre() != EstadoReservaNombre.ENTREGADA){
+            reserva.setFechaLimiteRecoger(LocalDate.now().plusDays(15));
             reserva.setEstado(estadoReserva);
             return reservaMapper.entityToDto(reservaRepository.save(reserva));
         }
 
-        reserva.setFechaLimiteRecoger(LocalDate.now().plusDays(15));
+        reserva.setFechaLimiteRecoger(null);
 
         Ejemplar ejemplarDisponible = 
             ejemplarRepository.findFirstByLibroAndEstadoOrderByIdAsc(reserva.getLibro(), buscarEstadoEjemplarPorNombre(EstadoEjemplarNombre.DISPONIBLE))
@@ -164,7 +148,34 @@ public class ReservaServiceImpl implements ReservaService{
         return reservaMapper.entityToDto(reservaRepository.save(reserva));
     }
 
-    /* FUNCIONES REUTILIZABLES */
+    @Override
+    @Transactional
+    public void eliminarReservaPorId(Long id) {
+
+        Reserva reserva = reservaRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Reserva no encontrada."));
+
+        reservaRepository.delete(reserva);
+    }
+
+     @Override
+    public ReservaResponse obtenerReservaPorId(Long id) {
+        return reservaMapper.entityToDto(reservaRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Reserva no encontrada"))
+        );
+    }
+
+    @Override
+    public List<ReservaResponse> obtenerReservas(Long estadoId) {
+        return reservaMapper.listEntityToListDto(reservaRepository.buscarPorParametros(estadoId));
+    }
+
+
+
+
+
+
+    /* ======================== FUNCIONES REUTILIZABLES =======================================*/
     private Reserva buscarReservaPorId(Long id){
         return reservaRepository.findById(id)
             .orElseThrow(() -> new ResourceNotFoundException("Reserva no encontrado."));
