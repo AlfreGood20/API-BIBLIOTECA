37f51f0d4ac2836df1ca0500e6207e426fdb122d
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-08-15 16:57:24 -0600

    DOCUMENTACION

--- src/main/java/com/api/biblioteca/controllers/AutorController.java
+++ src/main/java/com/api/biblioteca/controllers/AutorController.java
@@ -5,9 +5,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.api.biblioteca.dtos.request.AutorRequest;
 import com.api.biblioteca.dtos.response.AutorResponse;
 import com.api.biblioteca.services.AutorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,27 +20,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
 
-
-
-
 @RestController
-@RequestMapping("api/autores")
+@RequestMapping("/api/autores")
 @RequiredArgsConstructor
+@Tag(name = "Autores", description = "Operaciones para autores de libros.")
 public class AutorController {
 
     private final AutorService autorService;
 
-    @PostMapping("/")
+    @Operation(summary = "Crear nuevo autor para libro", description = "Creas nuevo autor para libro. Solo para administradores. ")
+    @PostMapping
     public ResponseEntity<AutorResponse> crearNuevo(@Valid @RequestBody AutorRequest request) {
         return new ResponseEntity<AutorResponse>(autorService.crearNuevo(request), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Obtener un autor para libro por id", description = "Obtienes un autor por id.")
     @GetMapping("/{id}")
     public ResponseEntity<AutorResponse> obtenerPorId(@PathVariable Long id) {
         return ResponseEntity.ok().body(autorService.obtenerAutorPorId(id));
     }
 
-    @GetMapping("/")
+    @Operation(summary= "Obtener autores de libros por filtros", description = "Obtienes todos los autores por diferentes filtros.")
+    @GetMapping
     public ResponseEntity<List<AutorResponse>> obetenerAutores(
         @RequestParam(required = false) String nombre,
         @RequestParam(required = false) String apellidoPaterno,
@@ -49,12 +51,13 @@ public class AutorController {
         return ResponseEntity.ok().body(autorService.obtenerAutores(nombre, apellidoPaterno, apellidoMaterno, nacionalidadId));
     }
     
-
+    @Operation(summary = "Actualizar autor de libro por id", description = "Actualizas un autor por id. Solo para administradores.")
     @PutMapping("/{id}")
     public ResponseEntity<AutorResponse> actulizarAutorPorId(@PathVariable Long id,@Valid AutorRequest request) {
         return ResponseEntity.ok().body(autorService.actualizarAutorPorId(request, id));
     }
 
+    @Operation(summary = "Eliminas autor de libro por id", description = "Eliminas un autor por id. Solo para administradores.")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> eliminarPorId(@PathVariable Long id){
         autorService.eliminarAutorPorId(id);
--- src/main/java/com/api/biblioteca/controllers/EditorialController.java
+++ src/main/java/com/api/biblioteca/controllers/EditorialController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.api.biblioteca.dtos.request.EditorialRequest;
 import com.api.biblioteca.models.Editorial;
 import com.api.biblioteca.services.EditorialService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import java.util.List;
@@ -16,29 +19,33 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
 @RestController
-@RequestMapping("api/editoriales")
+@RequestMapping("/api/editoriales")
 @RequiredArgsConstructor
+@Tag(name = "Editoriales", description = "Operaciones editoriales de libros.")
 public class EditorialController {
 
     private final EditorialService editorialService;
 
-    @PostMapping("/")
+    @Operation(summary = "Crear nuevo editorial para libros", description = "Creas un nuevo editorial. Solo para administradores.")
+    @PostMapping
     public ResponseEntity<Editorial> crearNuevo(@Valid @RequestBody EditorialRequest request) {
         return new ResponseEntity<Editorial>(editorialService.crearNuevo(request), HttpStatus.CREATED);
     }
 
-    @GetMapping("/")
+    @Operation(summary = "Obtener editoriales para libros", description = "Obtienes los editoriales.")
+    @GetMapping
     public ResponseEntity<List<Editorial>> obtenerEditoriales() {
         return ResponseEntity.ok().body(editorialService.obtenerEditoriales());
     }
     
+    @Operation(summary = "Obtener editorial para libro por id", description = "Obtienes un editorial por id ingresado.")
     @GetMapping("/{id}")
     public ResponseEntity<Editorial> obtenerPorId(@PathVariable Long id) {
         return ResponseEntity.ok().body(editorialService.obtenerEditorialPorId(id));
     }
 
+    @Operation(summary = "Eliminar editorial para libro por id", description = "Eliminas un editoial por id ingresado.")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> eliminarPorId(@PathVariable Long id){
         editorialService.eliminarEditorialPorId(id);
