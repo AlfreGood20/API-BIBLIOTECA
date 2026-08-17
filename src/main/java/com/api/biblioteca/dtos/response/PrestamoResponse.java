d21c765906cb6f7aa5f6f17a54936fcbba82e4f2
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-08-17 17:14:50 -0600

    CAMBIO DE ATRIBUTOS MENOS INFORMACION QUE DAR

--- src/main/java/com/api/biblioteca/dtos/response/PrestamoResponse.java
+++ src/main/java/com/api/biblioteca/dtos/response/PrestamoResponse.java
@@ -2,23 +2,17 @@ package com.api.biblioteca.dtos.response;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-
 import com.api.biblioteca.enums.EstadoPrestamoNombre;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record PrestamoResponse
 (
     Long id,
-    @JsonProperty("fecha_registro")
-    LocalDateTime fechaRegistro,
-    @JsonProperty("fecha_limite")
-    LocalDate fechaLimite,
-    @JsonProperty("fecha_devolucion")
-    LocalDate fechaDevolucion,
+    @JsonProperty("fecha_registro") LocalDateTime fechaRegistro,
+    @JsonProperty("fecha_limite") LocalDate fechaLimite,
+    @JsonProperty("fecha_devolucion") LocalDate fechaDevolucion,
     EstadoPrestamoNombre estado,
-    UsuarioResumen usuario,
-    @JsonProperty("usuario_admin")
-    UsuarioResumen usuarioAdmin,
+    UsuarioResumen autorizo,
     EjemplarResponse ejemplar
 ) {
 }
--- src/main/java/com/api/biblioteca/dtos/response/UsuarioResumen.java
+++ src/main/java/com/api/biblioteca/dtos/response/UsuarioResumen.java
@@ -1,21 +1,13 @@
 package com.api.biblioteca.dtos.response;
 
-import java.time.LocalDate;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record UsuarioResumen
 (
     Long id,
-    String nombre,
-    @JsonProperty("apellido_paterno")
-    String apellidoPaterno,
-    @JsonProperty("apellido_materno")
-    String apellidoMaterno,
-    @JsonProperty("fecha_nacimiento")
-    LocalDate fechaNacimiento,
-    String genero,
-    String curp,
+    String nombre, 
+    @JsonProperty("apellido_paterno") String apellidoPaterno,
+    @JsonProperty("apellido_materno") String apellidoMaterno,
     String correo
 ) {
 }
