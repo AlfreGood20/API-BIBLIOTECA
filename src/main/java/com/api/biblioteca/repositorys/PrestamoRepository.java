daed9ae58789871c2f44b23ab8f1ac52aa2b96e6
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-08-17 17:17:08 -0600

    CONSULTAR LOS PRESTAMOS DE UN USUARIO POR PAGINAS

--- src/main/java/com/api/biblioteca/repositorys/PrestamoRepository.java
+++ src/main/java/com/api/biblioteca/repositorys/PrestamoRepository.java
@@ -2,6 +2,9 @@ package com.api.biblioteca.repositorys;
 
 import java.time.LocalDate;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,21 +15,19 @@ import com.api.biblioteca.models.Usuario;
 
 public interface PrestamoRepository extends JpaRepository<Prestamo, Long> {
 
-    List<Prestamo> findByUsuario(Usuario usuario);
-
-    long countByUsuarioAndMultaIsNotNull(Usuario usuario);
-
+    @EntityGraph(attributePaths = {"ejemplar", "estado", "usuarioAdmin"})
     @Query("""
         SELECT p FROM Prestamo p
-        WHERE (:estadoId IS NULL OR p.estado.id = :estadoId)
-        AND (:usuarioAdminId IS NULL OR p.usuarioAdmin.id = :usuarioAdminId)
-        AND (:usuarioId IS NULL OR p.usuario.id = :usuarioId)
+        WHERE p.usuario = :usuario
+        AND (:estado IS NULL OR p.estado.nombre = :estado )
         """)
-    List<Prestamo> buscarPorParametros(
-        @Param("estadoId") Long estadoId,
-        @Param("usuarioAdminId") Long usuarioAdminId,
-        @Param("usuarioId") Long usuarioId
+    Page<Prestamo> misPrestamos(
+        @Param("usuario") Usuario usuario,
+        @Param("estado") EstadoPrestamoNombre estado,
+        Pageable Pageable
     );
+
+    List<Prestamo> findByUsuario(Usuario usuario);
 
     List<Prestamo> findByEstadoAndFechaLimiteBefore(EstadoPrestamo estado, LocalDate fecha);
 
