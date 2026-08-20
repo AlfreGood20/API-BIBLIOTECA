3e41fa43e3aaf05c4f2f979cee5301c70a7becbc
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-08-19 19:14:43 -0600

    BUSCAR POR USUARIO SU DIRECCION

--- src/main/java/com/api/biblioteca/repositorys/DireccionRepository.java
+++ src/main/java/com/api/biblioteca/repositorys/DireccionRepository.java
@@ -2,8 +2,12 @@ package com.api.biblioteca.repositorys;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.api.biblioteca.models.Direccion;
+import com.api.biblioteca.models.Usuario;
+import java.util.Optional;
+
 
 
 public interface DireccionRepository extends JpaRepository<Direccion, Long> {
 
+    Optional<Direccion> findByUsuario(Usuario usuario);
 }
